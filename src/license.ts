3c2847b65b54a009f05119db5d974a265fc8f7a4
vapormusic <vapormusic4@gmail.com>
2025-10-29 20:40:08 +0100

    fix

--- src/license.ts
+++ src/license.ts
@@ -239,11 +239,11 @@ export class Session {
     const keyContainers = license.key.map((keyContainer) => {
       if (keyContainer.type && keyContainer.key && keyContainer.iv) {
         const keyId = keyContainer.id ? Buffer.from(keyContainer.id).toString("hex") : "00000000000000000000000000000000";
-        const decipher = forge.cipher.createDecipher("AES-CBC", encKey.toString("binary"));
-        decipher.start({ iv: Buffer.from(keyContainer.iv).toString("binary") });
+        const decipher = forge.cipher.createDecipher("AES-CBC", forge.util.createBuffer(Buffer.from(encKey)));
+        decipher.start({ iv: forge.util.createBuffer(Buffer.from(keyContainer.iv)) });
         decipher.update(forge.util.createBuffer(Buffer.from(keyContainer.key)));
         decipher.finish();
-        const decryptedKey = Buffer.from(decipher.output.data, "binary");
+        const decryptedKey = Buffer.from(decipher.output.data);
         const key: KeyContainer = {
           kid: keyId,
           key: decryptedKey.toString("hex")
